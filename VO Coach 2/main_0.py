64509e075718dbf67fc2c4edadde9c409e38b13b
05Hercules <singh05deependra@gmail.com>
2024-06-17 10:53:01 +0530

    3 windows added accordingly

--- VO Coach 2/main_0.py
+++ VO Coach 2/main_0.py
@@ -5,6 +5,152 @@ from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QTextEdit, QVBoxLayout, QFileDialog,QListWidget
 import os
 
+class OverlayWindow_0(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        # self.load_text_from_file()  # Load text when initializing of sss
+        # self.load_text_from_file_2()
+
+    def initUI(self):
+        self.setWindowTitle('Overlay')
+        self.setGeometry(200, 100, 300, 300)
+        # |Qt.FramelessWindowHint
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setStyleSheet("background:rgba(0, 0, 0, 0.7);")
+        self.setWindowTitle("Window 2")
+
+#!Adding a button to go to next window
+
+#*Adding button1
+        self.button1 = QPushButton('Enter custom query', self) #Takes the user to next window
+        self.button1.clicked.connect(self.open_window_1 )
+        self.button1.setGeometry(10, 40, 280, 60)
+        self.button1.setStyleSheet("""
+            QPushButton {
+                background-color: #3537b4;  /* Green background */
+                color: white;               /* White text */
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                font-weight: 800;
+                border: none;               /* No border */
+                padding: 14px 26px;         /* Some padding */
+                text-align: center;         /* Centered text */
+                text-decoration: none;      /* No underline */
+                font-size: 16px;            /* Increase font size */
+                margin: 4px 2px;            /* Some margin */
+                border-radius: 6px;
+            }
+            QPushButton:hover {
+                background-color: white;    /* White background on hover */
+                color: black;               /* Black text on hover */
+                border: 2px solid #795dec;  /* Green border on hover */
+            }
+        """)
+
+#*Adding button2
+        self.button2 = QPushButton('Provide SSS', self)
+        self.button2.clicked.connect(self.open_window_2)
+        self.button2.setGeometry(10, 150, 280, 60)
+        self.button2.setStyleSheet("""
+            QPushButton {
+                background-color: #b841c1;  /* Green background */
+                color: white;               /* White text */
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                font-weight: 800;
+                border: none;               /* No border */
+                padding: 14px 26px;         /* Some padding */
+                text-align: center;         /* Centered text */
+                text-decoration: none;      /* No underline */
+                font-size: 16px;            /* Increase font size */
+                margin: 4px 2px;            /* Some margin */
+                border-radius: 6px;
+            }
+            QPushButton:hover {
+                background-color: white;    /* White background on hover */
+                color: black;               /* Black text on hover */
+                border: 2px solid #795dec;  /* Green border on hover */
+            }
+        """)
+
+#*Adding button3 for exiting the window
+        self.button3 = QPushButton('Quit', self)
+        self.button3.clicked.connect(self.exit_window)
+        self.button3.setGeometry(10, 240, 280, 60)
+        self.button3.setStyleSheet("""
+            QPushButton {
+                background-color: #800000;  /* Green background */
+                color: white;               /* White text */
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                font-weight: 800;
+                border: none;               /* No border */
+                padding: 14px 26px;         /* Some padding */
+                text-align: center;         /* Centered text */
+                text-decoration: none;      /* No underline */
+                font-size: 16px;            /* Increase font size */
+                margin: 4px 2px;            /* Some margin */
+                border-radius: 6px;
+            }
+            QPushButton:hover {
+                background-color: black;    /* White background on hover */
+                color: red;               /* Black text on hover */
+                border: 2px solid red;  /* Green border on hover */
+            }
+        """)
+
+#!Function to quit the window
+    def exit_window(self):
+        sys.exit(app.exec_())
+
+#!Function to load output in the Window-2 textbox
+    # def load_text_from_file(self):
+    #     Text_Folder = "./"
+    #     file_path = os.path.join(Text_Folder, "output.txt")
+
+    #     if os.path.exists(file_path):
+    #         with open(file_path, "r") as file:
+    #             content = file.read()
+    #             self.textbox_2.setText(content)
+    #     else:
+    #         self.textbox_2.setText("No input file found.")
+
+#!Function to load output in the Window-2 textbox
+    # def load_text_from_file_2(self):
+    #     Text_Folder = "./"
+    #     file_path = os.path.join(Text_Folder, "output_Assistant.txt")
+
+    #     if os.path.exists(file_path):
+    #         with open(file_path, "r") as file:
+    #             content = file.read()
+    #             self.textbox_2.setText(content)
+    #     else:
+    #         self.textbox_2.setText("No input file found.")
+
+    
+#!Colors of the window
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setBrush(QColor(0, 0, 0, 120))  # Changing the color of bg window
+        painter.drawRect(self.rect())
+
+        self.show()
+ 
+#! Function to open Overlay_window_1
+    def open_window_1(self):
+        self.window2 = OverlayWindow_1()  # Create an instance of Window1
+        self.window2.show()
+        self.hide()
+
+#! Function to open Overlay_window_3
+    def open_window_2(self):
+        self.window2 = OverlayWindow_2()  # Create an instance of Window1
+        self.window2.show()
+        self.hide()
+
+#!-------------------------------------------------------------------------------- OVERLAY_WINDOW_1------------------------------------------------------------------------------!#
+#!-------------------------------------------------------------------------------- OVERLAY_WINDOW_1-----------------------------------------------------------------------------!#
+#!-------------------------------------------------------------------------------- OVERLAY_WINDOW_1-----------------------------------------------------------------------------!#
 
 class OverlayWindow_1(QMainWindow):
     def __init__(self):
@@ -24,6 +170,30 @@ class OverlayWindow_1(QMainWindow):
         self.button1 = QPushButton('Window 2', self)
         self.button1.clicked.connect(self.open_window_2)
         self.button1.setGeometry(80, 100, 200, 60)
+        self.button1.setStyleSheet("""
+            QPushButton {
+                background-color: #4CAF50;  /* Green background */
+                color: white;               /* White text */
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                font-weight: 800;
+                border: none;               /* No border */
+                padding: 14px 26px;         /* Some padding */
+                text-align: center;         /* Centered text */
+                text-decoration: none;      /* No underline */
+                font-size: 16px;            /* Increase font size */
+                margin: 4px 2px;            /* Some margin */
+                border-radius: 6px;
+            }
+            QPushButton:hover {
+                background-color: white;    /* White background on hover */
+                color: black;               /* Black text on hover */
+                border: 2px solid #4CAF50;  /* Green border on hover */
+            }
+        """)
+#* Adding button1
+        self.button1 = QPushButton('Window 0', self)
+        self.button1.clicked.connect(self.open_window_0)
+        self.button1.setGeometry(80, 170, 200, 60)
         self.button1.setStyleSheet("""
             QPushButton {
                 background-color: #4CAF50;  /* Green background */
@@ -205,6 +375,11 @@ class OverlayWindow_1(QMainWindow):
     #     # Not able to clear the textbar is one major problem
     #     self.save_input()
     #     self.run_script_text()
+#!Function to shift back to window_1
+    def open_window_0(self):
+        self.window1 = OverlayWindow_0()  # Create an instance of Window2
+        self.window1.show()
+        self.hide()
 
 #!Function for calling combined sss and user query input
     def userInput_and_sss(self):
@@ -263,7 +438,9 @@ class OverlayWindow_1(QMainWindow):
         self.hide()
 
 
-#!----------------------------------------Defining another class: Window-2 to execute it upon a button click------------------------------------------------------!#
+#!--------------------------------------------------------------------------------------OVERLAY_WINDOW-2-------------------------------------------------------------------------!#
+#!--------------------------------------------------------------------------------------OVERLAY_WINDOW-2-------------------------------------------------------------------------!#
+#!--------------------------------------------------------------------------------------OVERLAY_WINDOW-2-------------------------------------------------------------------------!#
 
 class OverlayWindow_2(QMainWindow):
     def __init__(self):
@@ -280,7 +457,6 @@ class OverlayWindow_2(QMainWindow):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setStyleSheet("background:rgba(0, 0, 0, 0.7);")
         self.setWindowTitle("Window 2")
-        # Add any other widgets or functionality for Window2 here
 
 #!Adding a button to go back to previous window
 
@@ -494,6 +670,6 @@ class OverlayWindow_2(QMainWindow):
 #!Showing the final window
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    overlay = OverlayWindow_1()
+    overlay = OverlayWindow_0()
     overlay.show()
     sys.exit(app.exec_())
